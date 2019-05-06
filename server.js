70559c942b3a9cbfebaafd1665986e7aef0671c7
Esterling Accime <esterling.accime@coxautoinc.com>
2019-05-06 00:58:32 -0400

    adding support for cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
+const cors = require('cors');
 const passport = require('./passport');
 const authenticationRoute = require('./routes/authentication');
 const apiRoutes = require('./routes/apiRoutes');
@@ -16,6 +17,7 @@ app.use(express.urlencoded({
     extended: false,
 }));
 app.use(express.json());
+app.use(cors());
 app.use(cookieSession({
     maxAge: process.env.MAX_AGE || 24 * 60 * 60 * 1000, // one day in miliseconds
     name: 'session',
